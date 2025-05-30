22b4b682b606a605cac7efc4aef4696fee12e7a6
Namanpn2126 <naman.n@s.amity.edu>
2025-05-30 16:49:21 +0530

    commit changed in script|script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Main JavaScript file
+// superb coding skills of sujay and i think he will get good package of 1cr
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Document ready!');
 }); 
