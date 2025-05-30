92a64b2afe1d9de98660d9e9769ae07e31930081
NNRT21 <nikunj.thrupthi@s.amity.edu>
2025-05-30 15:30:54 +0530

    js changes|script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// Main JavaScript file
+// Main JavaScript file !!
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Document ready!');
 }); 
+// all the js is in index page 
+// so this file is for project only .
+// bye!!
